0c70c9d16fada764c34f66b6626aae7325d5a239
erik schievenin <erik.schievenin@gmail.com>
2022-05-16 17:39:29 +0200

    start exercise

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+class CreditCard{
+    public $number;
+    public $expiration_date;
+    public $balance;
+
+    /*
+    * crea una carta di credito
+    * 
+    * @param float $number è il numero dell carta di credito
+    * @param int $expiration_date è la scadenza della carta di credito
+    * @param int $balance è il saldo corrente della carta di credito
+    *
+    */
+
+
+
+}
+
+?>
